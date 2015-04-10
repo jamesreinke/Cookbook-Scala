125923a0e395253182b9fd15b388cf1f41c0644e
James Reinke <james@rtsystems.io>
2015-04-09 21:10:11 -0700

    ~

--- src/main/scala/Fifteen.scala
+++ src/main/scala/Fifteen.scala
@@ -15,6 +15,21 @@ object Fifteen {
 		operator: String,
 		left: Expr, 
 		right: Expr) extends Expr
+
+	/*
+	Case classes automatically add factory methods.
+	*/
+	val v = Var("x") // no 'new' required
+	println(v.name) // all parameters are automatically instantiated with a var
+	val op = BinOp("+", Number(1), v)
+	println(op.left)
+	println(op.right == Var("x")) // should return true
+	var opCopy = op.copy(operator = "-") // automatic implementation of the copy method
+	println("")
+
+	/*
+	Ways to match!
+	*/
 	def simplifyTop(expr: Expr): Expr = expr match {
 		case UnOp("-", UnOp("-", e)) => e
 		case BinOp("+", e, Number(0)) => e
@@ -29,21 +44,46 @@ object Fifteen {
 		// we could also use _ for the wildcard
 		case somethingElse => "no pattern matched for object " + somethingElse
 	}
+
+	/*
+	deep case match
+	*/
+	val expr: Any = Nil
 	expr match {
 		// three element list starting with 0
 		case List(0, _, _) => println("found it!")
+		// matches any length list with starting element 0
+		case List(0, _*) => println("kind of found it")
 		case _ => println("not found")
 	}
 
+	/*
+	this does not work; scala, like java uses the erasure model of generics
+	we can only match Map[Any, Any]
+	*/
+	def isIntIntMap(x: Any): Boolean = x match {
+		case m: Map[Int, Int] => true
+		case _ => false
+	}
 
 	/*
-	Case classes automatically add factory methods.
+	Variable Binding
 	*/
-	val v = Var("x") // no 'new' required
-	println(v.name) // all parameters are automatically instantiated with a var
-	val op = BinOp("+", Number(1), v)
-	println(op.left)
-	println(op.right == Var("x")) // should return true
-	var opCopy = op.copy(operator = "-") // automatic implementation of the copy method
-	println("")
+	expr match {
+		// pattern matching while making a variable available 
+		case UnOp("abs", e @ UnOp("abs", _)) => e
+		case _ => expr
+	}
+
+	/*
+	Pattern Guards
+	*/
+	def simplifyAdd(e: Expr) = e match {
+		case BinOp("+", x, y) if x == y =>
+			BinOp("*", x, Number(2))
+		case _ => e
+	}
+
+
+
 }
