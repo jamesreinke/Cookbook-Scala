a4adaed3af4d5853dd5580135ecaf61cbd7e2da8
James Reinke <james@rtsystems.io>
2015-04-09 19:05:26 -0700

    ~

--- src/main/scala/Fifteen.scala
+++ src/main/scala/Fifteen.scala
@@ -15,13 +15,21 @@ object Fifteen {
 		operator: String,
 		left: Expr, 
 		right: Expr) extends Expr
+	def simplifyTop(expr: Expr): Expr = expr match {
+		case UnOp("-", UnOp("-", e)) => e
+		case BinOp("+", e, Number(0)) => e
+		case BinOp("*", e, Number(1)) => e
+		case _ => expr
+	}
+
+
 	/*
 	Case classes automatically add factory methods.
 	*/
 	val v = Var("x") // no 'new' required
-	v.name // all parameters are automatically instantiated with a var
+	println(v.name) // all parameters are automatically instantiated with a var
 	val op = BinOp("+", Number(1), v)
-	op.left
-	op.right == Var("x") // should return true
+	println(op.left)
+	println(op.right == Var("x")) // should return true
 	var opCopy = op.copy(operator = "-") // automatic implementation of the copy method
 }
